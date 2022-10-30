0bd284196bc3a85a0b09e436cfb5f088ebb506a9
ProfElements <profelements1@gmail.com>
2022-10-30 05:40:23 -0500

    :bug: fix(nvim): Update to use default_capabilities instead of update_capabilties

--- nvim/lua/profelements/lsp.lua
+++ nvim/lua/profelements/lsp.lua
@@ -37,5 +37,5 @@ require("lspconfig").rust_analyzer.setup{
 
     end,
     cmd = {"rustup", "run", "nightly", "rust-analyzer"},
-    capabilities = require("cmp_nvim_lsp").update_capabilities(vim.lsp.protocol.make_client_capabilities()),
+    capabilities = require("cmp_nvim_lsp").default_capabilities(vim.lsp.protocol.make_client_capabilities()),
 }
